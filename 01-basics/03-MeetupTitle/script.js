4475592f78516899a91845d4007604df9ad58aa8
Nikita Kinash <nka2018nn@gmail.com>
2021-05-18 00:46:13 +0300

    01-basics/03-MeetupTitle

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,30 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // Требуется создать Vue приложение
+/**
+ * Не стесняйтесь разворачивать на кодревью))
+ * Хочу узнать, Как пишут на Vue нормальные люди)
+ * @type {Vue}
+ */
+const app = new Vue({
+  data() {
+    return {
+      checked: 0,
+      title: ''
+    };
+  },
+  watch: {
+    checked(value) {
+      this.getTitle(value);
+    }
+  },
+  methods: {
+    getTitle(item) {
+      fetch('https://course-vue.javascript.ru/api/meetups/' + item)
+        .then((res) => res.json())
+        .then(res => this.title = res.title);
+    }
+  }
+});
+
+app.$mount('#app');
