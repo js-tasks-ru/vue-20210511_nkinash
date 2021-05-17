025d6fc4fb0039bde4a0a91ce9adfb8388ca4101
Nikita Kinash <nka2018nn@gmail.com>
2021-05-17 23:13:17 +0300

    01-basics/01-CounterButtonDraft

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,18 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // Требуется создать Vue приложение
+
+const app = new Vue({
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    click() {
+      this.counter++;
+    }
+  }
+});
+
+app.$mount('#app');
