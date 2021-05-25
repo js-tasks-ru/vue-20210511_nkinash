9ee81074c335c39c10658ac1f827be3359def53e
Nikita Kinash <nka2018nn@gmail.com>
2021-05-25 22:03:49 +0300

    02-components/04-MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,49 @@
 export default {
   name: 'MeetupInfo',
-
+  props: {
+    organizer: {
+      required: true,
+      type: String
+    },
+    place: {
+      required: true,
+      type: String
+    },
+    date: {
+      required: true,
+      type: Date
+    },
+  },
+  computed: {
+    dateOnlyString() {
+      return new Date(this.date).toISOString().split('T')[0]
+    },
+    formatDate() {
+      return new Date(this.date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    }
+  },
+  methods: {
+    icon(type) {
+      return `/assets/icons/icon-${type}.svg`;
+    },
+  },
   template: `
     <ul class="info-list">
       <li>
-        <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        <img class="icon info-list__icon" alt="icon" :src="icon('user')" />
+        {{ organizer }}
       </li>
       <li>
-        <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        <img class="icon info-list__icon" alt="icon" :src="icon('map')" />
+        {{ place }}
       </li>
       <li>
-        <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <img class="icon info-list__icon" alt="icon" :src="icon('cal-lg')" />
+        <time :datetime="dateOnlyString">{{ formatDate }}</time>
       </li>
     </ul>`,
 };
