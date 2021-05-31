dca090892f07cc09b00f14dee1a155ba69af0a8a
Кинаш Никита <kinash@cmd.su>
2021-06-01 00:58:18 +0300

    02-components/07-MeetupPageComponent

--- 02-components/07-MeetupPageComponent/MeetupPage.js
+++ 02-components/07-MeetupPageComponent/MeetupPage.js
@@ -1,10 +1,30 @@
-// import MeetupView from './MeetupView.js';
-// import { MEETUP_ID, fetchMeetup } from './data.js';
+import MeetupView from './../06-MeetupView/MeetupView.js';
+import { MEETUP_ID, fetchMeetup } from './data.js';
 
 const MeetupPage = {
   name: 'MeetupPage',
+  components: {
+    MeetupView
+  },
+  data() {
+    return {
+      meetupID: MEETUP_ID,
+      meetup: null
+    }
+  },
+  methods: {
+    getMeetupData() {
+      fetchMeetup(this.meetupID)
+        .then(res => this.meetup = res)
+    },
+  },
+  mounted() {
+    this.getMeetupData()
+  },
 
-  template: `<div></div>`,
+  template: `<div>
+    <meetup-view v-if="meetup" :meetup="meetup"/>
+  </div>`,
 };
 
 export default MeetupPage;
