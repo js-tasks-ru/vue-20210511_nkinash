b17036ae1d1110fbd321ffa09b5f3f0828401e67
Кинаш Никита <kinash@cmd.su>
2021-06-18 23:13:26 +0300

    02-components/08-MeetupsCalendar/Оптимизация

--- 02-components/08-MeetupsCalendar/MeetupsCalendar.js
+++ 02-components/08-MeetupsCalendar/MeetupsCalendar.js
@@ -1,22 +1,26 @@
 /*
-  Полезные функции по работе с датой можно описать вне Vue компонента
+ * Соотносим митапы с календарем по дате в формате ISO
  */
 
 function daysInMonth(date) {
   return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 }
 
+function dateIndex(date) {
+  return  date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
+}
+
 function getMonthArray(date) {
   let monthArray = [],
-      daysInCurMonth = daysInMonth(date);
+    daysInCurMonth = daysInMonth(date);
 
   for (let day = 1; day <= daysInCurMonth; ++day) {
     monthArray.push((new Date(date.getFullYear(), date.getMonth(), day)));
   }
 
   if (monthArray[0].getDay() !== 1) {
-    let prevMonthDate = new Date(date.getFullYear(), date.getMonth() - 1, 1),
-        prevDaysCounter = daysInMonth(prevMonthDate);
+    let prevMonthDate = new Date(date.getFullYear(), date.getMonth() - 1, 15),
+      prevDaysCounter = daysInMonth(prevMonthDate);
     while (monthArray[0].getDay() > 1) {
       monthArray.unshift(new Date(prevMonthDate.getFullYear(), prevMonthDate.getMonth(), prevDaysCounter));
       prevDaysCounter = prevDaysCounter - 1;
@@ -24,10 +28,10 @@ function getMonthArray(date) {
   }
 
   if (monthArray[monthArray.length - 1].getDay() !== 0) {
-    let nextMonthDate = new Date(date.getFullYear(), date.getMonth() + 1, 1),
-        nextDaysCounter = daysInMonth(nextMonthDate);
-    while (monthArray[monthArray.length - 1].getDay() !== 0) {
-      monthArray.push(new Date(date.getFullYear(), date.getMonth(), nextDaysCounter));
+    let nextMonthDate = new Date(date.getFullYear(), date.getMonth() + 1, 15),
+      nextDaysCounter = 1;
+    while (monthArray[monthArray.length - 1].getDay() > 0) {
+      monthArray.push(new Date(nextMonthDate.getFullYear(), nextMonthDate.getMonth(), nextDaysCounter));
       nextDaysCounter++;
     }
   }
@@ -45,24 +49,52 @@ const MeetupsCalendar = {
   },
   data() {
     return {
-      date: null
+      date: new Date(),
+      meetupsByDate: null
     }
   },
+  mounted() {
+    this.setMeetupsByDate()
+  },
   computed: {
-    monthArray() {
-      return getMonthArray(this.date)
-    },
     formatMonth() {
       return this.date.toLocaleString(navigator.language, {
         month: 'long',
       }) + ' ' + this.date.getFullYear();
+    },
+    monthArray() {
+      let month = getMonthArray(this.date),
+          finalArray = [],
+          meetupsByDate = this.meetupsByDate;
+
+      month.forEach(day => {
+        let meetupsThisDay = meetupsByDate && meetupsByDate[dateIndex(day)] ?
+              meetupsByDate[dateIndex(day)].meetups : [];
+
+        finalArray.push({
+          date: day,
+          meetups: meetupsThisDay
+        })
+      });
+
+      return finalArray;
     }
   },
-  created() {
-    let currDate = new Date();
-    this.date = new Date(currDate.getFullYear(), currDate.getMonth(), 1)
-  },
   methods: {
+    setMeetupsByDate() {
+      let meetupsByDate = {};
+
+      this.meetups.forEach(meetup => {
+        let mdate = new Date(meetup.date);
+          if (meetupsByDate[dateIndex(mdate)]) {
+            meetupsByDate[dateIndex(mdate)].meetups.push(meetup);
+          } else {
+            meetupsByDate[dateIndex(mdate)] = { meetups: [meetup] };
+          }
+      });
+
+      this.meetupsByDate = meetupsByDate;
+    },
     addMonth() {
       let month = this.date.getMonth() + 1;
       this.date.setDate(3)
@@ -73,40 +105,31 @@ const MeetupsCalendar = {
       this.date.setDate(3)
       this.date = new Date(this.date.setMonth(month))
     },
-    getMeetupsTonight(date) {
-      return this.meetups.filter((meetup) => {
-        let mdate = new Date(meetup.date);
-        return mdate.getMonth() === date.getMonth()
-          && mdate.getDate() === date.getDate()
-          && mdate.getFullYear() === date.getFullYear()
-      })
-    },
   },
-  template: `<div class="rangepicker">
-    <div class="rangepicker__calendar">
-      <div class="rangepicker__month-indicator">
-        <div class="rangepicker__selector-controls">
-          <button class="rangepicker__selector-control-left" @click="substrMonth"></button>
-          <div>{{ formatMonth }}</div>
-          <button class="rangepicker__selector-control-right" @click="addMonth"></button>
+  template: `
+    <div class="rangepicker">
+      <div class="rangepicker__calendar">
+        <div class="rangepicker__month-indicator">
+          <div class="rangepicker__selector-controls">
+            <button class="rangepicker__selector-control-left" @click="substrMonth"></button>
+            <div>{{ formatMonth }}</div>
+            <button class="rangepicker__selector-control-right" @click="addMonth"></button>
+          </div>
+        </div>
+        <div class="rangepicker__date-grid">
+          <div class="rangepicker__cell"
+               v-for="(day, index) in monthArray"
+               :class="{ rangepicker__cell_inactive: day.date.getMonth() !== date.getMonth()}"
+               :key="index"
+               >
+            {{ day.date.getDate() }}
+            <a class="rangepicker__event" v-for="meetup in day.meetups">
+              {{ meetup.title }}
+            </a>
+          </div>
         </div>
       </div>
-      <div class="rangepicker__date-grid">
-        <template v-for="curDay in monthArray">
-          <div class="rangepicker__cell"
-               :class="{ rangepicker__cell_inactive: curDay.getMonth() !== date.getMonth()}">
-            {{ curDay.getDate() }}
-            <template v-if="getMeetupsTonight(curDay).length">
-              <a class="rangepicker__event" v-for="meetup in getMeetupsTonight(curDay)">
-                {{ meetup.title }}
-              </a>
-
-            </template>
-          </div>
-        </template>
-      </div>
-    </div>
-  </div>`,
+    </div>`,
 };
 
 export default MeetupsCalendar;
